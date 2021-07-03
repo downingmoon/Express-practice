2659c9658db959f7ddc9af65316adafc58836d07
downing <joy97q12@naver.com>
2021-07-04 04:28:14 +0900

    login & logout

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ var morgan = require('morgan')
 var mysql = require('mysql')
 var bodyParser = require('body-parser')
 var session = require('express-session')
+var cookieParser = require('cookie-parser')
 
 // Middleware Settings
 app.use(morgan('short'))
 app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Cookie
+app.use(cookieParser())
+
+// Session
+app.use(session({
+	key: 'downing-session',
+	secret: 'downing session secret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24
+	}
+}))
 
 // Router
 var loginRouter = require('./router/login-router.js')
@@ -18,13 +33,6 @@ app.use(loginRouter)
 // Ejs
 app.set('view engine', 'ejs')
 app.set('views', './views')
-
-// Session
-app.use(session({
-	secret: 'downing session secret',
-	resave: false,
-	saveUninitialized: true
-}))
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
--- router/login-router.js
+++ router/login-router.js
@@ -1,18 +1,21 @@
 var express = require('express');
-var router = express.Router()
 var bodyParser = require('body-parser');
 const db = require('../settings/database.js');
 const connection = require('../settings/database.js');
 var resModel = require('../public/response-model.js')
 var consts = require('../public/constants.js')
 
+var router = express.Router()
 router.use(bodyParser.urlencoded({extended: false}))
 
 router.get('/login', (req, res) => {
     console.log('Login Page')
-    res.render('./login/login', {
-        foo: 'bar'
-    })
+    var sess = req.session
+    if(sess.isLoggedin) {
+        res.redirect('/')
+    } else {
+        res.render('./login/login', {})
+    }
     // fs.readFile('./login/login.html', 'UTF-8', (err, data) => {
     //     res.send(ejs.render(data, {
     //         data: null,
@@ -36,10 +39,10 @@ router.post('/login', (req, res) => {
             let user = row[0]
             if(body.password == user.USER_PASSWORD) {
                 let sess = req.session
-                
                 sess.isLoggedin = true
                 sess[consts.USER_SESSION_INFO] = row[0]
                 console.log('#### SESS : ', sess)
+                
                 res.send(resModel.getResponseModel(true))
             } else {
                 res.send(resModel.getResponseModel(false, '존재하지 않거나 비밀번호가 잘못되었습니다.'))
@@ -47,6 +50,14 @@ router.post('/login', (req, res) => {
         }
     })
     
+})
+
+router.get('/logout', (req, res) => {
+    let sess = req.session
+    if(sess.isLoggedin) {
+        sess.destroy()
+    }
+    res.redirect('/login')
 })
 
 module.exports = router
